8f8440c5d0c81a1b0db5b55a9740a343e4f2a85a
MadMatix <madmatix.pl@gmail.com>
2021-03-05 23:46:25 +0100

    Languages - Best translations are from users, so we can trust you.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    /**
+     * @module EN
+     * @description English (English) Language
+     */
+    en: require('./lang/en.json'),
+
+    /**
+     * @module PL
+     * @description Polish (Polski) Language
+     */
+    pl: require('./lang/pl.json')
+}
